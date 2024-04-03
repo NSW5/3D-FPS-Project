38cc4f7b3b8f73cfbb1c0ce4a4c9219a542332c6
NSW5 <146126932+NSW5@users.noreply.github.com>
2024-04-03 11:57:29 -0700

    Ammo Counter

--- 3D FPS Project/Assets/Scripts/GameManager.cs
+++ 3D FPS Project/Assets/Scripts/GameManager.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public TextMeshProUGUI targetText;
+    private int _targetAmount;
+
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        int floatingTargetAmount = GameObject.FindGameObjectsWithTag("Floating Targets").Length;
+        int standingTargetAmount = GameObject.FindGameObjectsWithTag("Standing Targets").Length;
+        _targetAmount = floatingTargetAmount + standingTargetAmount;
+        targetText.text = "Targets: " + _targetAmount.ToString();
     }
 
     // Update is called once per frame
--- 3D FPS Project/Assets/Scripts/Target.cs
+++ 3D FPS Project/Assets/Scripts/Target.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Target : MonoBehaviour
 {
+    public TextMeshProUGUI targetText;
+    private int _targetAmount;
+
+    void Start()
+    {
+        this.gameObject.SetActive(true);
+        _targetAmount = GameObject.FindGameObjectsWithTag("Floating Target").Length;
+        targetText.text = "Targets:" + _targetAmount.ToString();
+    }
 
     void OnCollisionEnter(Collision other)
     {
@@ -12,7 +22,8 @@ public class Target : MonoBehaviour
             //add points to the score
             if(this.gameObject.tag == "Floating Target")
             {
-                Destroy(this.gameObject);
+                //Destroy(this.gameObject);
+                this.gameObject.SetActive(false);
             }
             //add grayscale to standing target later on
         }
